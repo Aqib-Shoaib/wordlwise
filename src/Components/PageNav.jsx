ed07b32c6ec0ff248a8d3780bdefef180c5f74ae
Aqib-Shoaib <118466147+Aqib-Shoaib@users.noreply.github.com>
2024-04-12 10:25:14 +0500

    update-2

--- src/Components/PageNav.jsx
+++ src/Components/PageNav.jsx
@@ -1,5 +1,20 @@
+import { NavLink } from "react-router-dom";
 function PageNav() {
-  return <div>navigation</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">product</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default PageNav;
